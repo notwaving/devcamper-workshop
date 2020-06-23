7432edbcb35d57e3bd34c15307c4d6174114c955
Samantha Burke <samanthaburke.uk@gmail.com>
2020-06-23 19:18:37 +0100

    creates custom middleware for logging, before importing and implementing morgan

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -13,11 +13,13 @@ dotenv.config({
 // Initiallise app with express
 const app = express();
 
-app.use(logger);
+// Dev logging middleware, only to be run in dev environment
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
-
 
 const PORT = process.env.PORT || 5000;
 
