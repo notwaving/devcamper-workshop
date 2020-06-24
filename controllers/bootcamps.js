7ac54de659a0f4c236af2e8c62b3beb411143286
Samantha Burke <samanthaburke.uk@gmail.com>
2020-06-24 19:39:51 +0100

    adds count functionality to getBootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,6 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
 
     res.status(200).json({
       success: true,
+      count: bootcamps.length,
       data: bootcamps
     });
   } catch (err) {
