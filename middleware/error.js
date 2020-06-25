abc2a2ae85bbd005740614d3a2f2d0b5053d205c
Samantha Burke <samanthaburke.uk@gmail.com>
2020-06-25 15:29:05 +0100

    creates and implements duplicate key error handler

--- middleware/error.js
+++ middleware/error.js
@@ -8,12 +8,18 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
-  //Monoose bad ObjectID
+  //Mongoose bad ObjectID
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value submitted`;
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
