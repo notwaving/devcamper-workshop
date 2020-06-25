e815a13fd18427331b81db2f780453ab80f5f75a
Samantha Burke <samanthaburke.uk@gmail.com>
2020-06-25 14:09:29 +0100

    creates and implements ErrorResponse, refactors controllers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,9 +15,7 @@ exports.getBootcamps = async (req, res, next) => {
       data: bootcamps
     });
   } catch (err) {
-    res.status(400).json({
-      success: false
-    })
+    next(err);
   }
 }
 
@@ -38,9 +36,10 @@ exports.getBootcamp = async (req, res, next) => {
       success: true,
       data: bootcamp
     });
+
     // Handle incorrectly formatted ID
   } catch (err) {
-    next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    next(err);
   }
 }
 
@@ -57,11 +56,8 @@ exports.createBootcamp = async (req, res, next) => {
     });
 
   } catch (err) {
-    res.status(400).json({
-      success: false
-    })
+    next(err);
   }
-
 };
 
 // @desc      Update bootcamp
@@ -76,18 +72,14 @@ exports.updateBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({
-        success: false
-      })
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     res.status(200).json({
       success: true,
       data: bootcamp
     });
   } catch (err) {
-    res.status(400).json({
-      success: false
-    });
+    next(err);
   }
 
 }
@@ -100,9 +92,7 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({
-        success: false
-      })
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     res.status(200).json({
       success: true,
@@ -110,8 +100,6 @@ exports.deleteBootcamp = async (req, res, next) => {
       data: {}
     });
   } catch (err) {
-    res.status(400).json({
-      success: false
-    });
+    next(err);
   }
 }
