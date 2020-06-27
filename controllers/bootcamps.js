689ad3d1951d9be2bed35b2f62dd1321a991881f
Samantha Burke <samanthaburke.uk@gmail.com>
2020-06-27 16:02:02 +0100

    creates getBootcampInRadius method in controllers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
-
-
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
@@ -85,4 +84,42 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     // sends back an empty object
     data: {}
   });
+});
+
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const {
+    zipcode,
+    distance
+  } = req.params;
+
+  // Get lat/long from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const long = loc[0].longitude;
+
+  // Calculate radius using radians
+  // Divide distance by radius of Earth
+  // Earth radius = 3963 miles / 6378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: {
+      $geoWithin: {
+        $centerSphere: [
+          [long, lat], radius
+        ]
+      }
+    }
+  })
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps
+  })
+
+
 });
