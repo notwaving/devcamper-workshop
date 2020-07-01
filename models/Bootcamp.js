c897ef18c51f07669dfd4b4df8bd1ab1d2d92b5a
Samantha Burke <samanthaburke.uk@gmail.com>
2020-07-01 18:31:24 +0100

    adds cascade delete functionality to courses

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -138,6 +138,13 @@ BootcampSchema.pre('save', async function (next) {
   next();
 });
 
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  await this.model('Course').deleteMany({
+    bootcamp: this._id
+  });
+})
+
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
