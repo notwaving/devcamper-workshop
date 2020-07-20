148fbe853243c741777c985f0c80cab3454b4564
Samantha Burke <samanthaburke.uk@gmail.com>
2020-07-20 19:34:07 +0100

    adds users to seeder

--- seeder.js
+++ seeder.js
@@ -11,8 +11,9 @@ dotenv.config({
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
-// Connect to db
+// Connect to database
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -25,11 +26,14 @@ const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
-// Import into db
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+
+// Import into database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log('Data Imported...'.green.inverse);
     process.exit();
@@ -41,10 +45,11 @@ const importData = async () => {
 // Delete data
 const deleteData = async () => {
   try {
-    // deleteMany() is a Mongoogse command. 
+    // deleteMany() is a Mongoose command. 
     //If no args are passed, everything is deleted.
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
