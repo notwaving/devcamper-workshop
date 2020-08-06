4028681bc0371c18ec3f3982e7064b03c5448d6d
notwaving <samanthaburke.uk@gmail.com>
2020-08-06 16:41:24 +0100

    adds reviews to seeder

--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,11 +23,21 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Read JSON files
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
 
-const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
-const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 
 // Import into database
 const importData = async () => {
@@ -34,29 +45,31 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 // Delete data
 const deleteData = async () => {
   try {
-    // deleteMany() is a Mongoose command. 
+    // deleteMany() is a Mongoose command.
     //If no args are passed, everything is deleted.
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 if (process.argv[2] === '-i') {
   importData();
