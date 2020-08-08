88dfca0ce3bb2c1fa24bec7c09bef483016fdf32
notwaving <samanthaburke.uk@gmail.com>
2020-08-08 18:23:43 +0100

    adds code to clear token cookie - currently commented out

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,12 +7,16 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // Set token from bearer token in header
     token = req.headers.authorization.split(' ')[1];
+    // Set token from cookie
   }
-
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token
+  //  else if (req.cookies.token) {
+  //   token = req.cookies.token;
   // }
 
   // Make sure token exists
@@ -38,8 +42,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return next(new ErrorResponse(`User role ${req.user.role} is not authorised to access this route`, 403));
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorised to access this route`,
+          403
+        )
+      );
     }
     next();
-  }
-}
+  };
+};
